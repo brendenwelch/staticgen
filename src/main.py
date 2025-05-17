36e6931f861377428e79b7364b5815e7df1cc753
brendenwelch <aloofd@gmail.com>
2025-05-17 16:54:51 -0700

    setup workstation

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import textnode
+
+
+def main():
+    print("hello world")
+    node = textnode.TextNode("idk man, some text", textnode.TextType.NORMAL, "httpxd")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
