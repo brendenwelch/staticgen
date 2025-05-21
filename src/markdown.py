f7cc5940b535c565713cda6b407dd92fafc67090
brendenwelch <aloofd@gmail.com>
2025-05-21 10:35:59 -0700

    finally produce parent and all children nodes. Tests not passing due to circular dependencies

--- src/markdown.py
+++ src/markdown.py
@@ -1,4 +1,6 @@
 import re
+from textnode import TextNode, BlockType, block_to_block_type, text_node_to_html_node
+from htmlnode import HTMLNode
 
 
 def extract_markdown_images(text):
@@ -37,3 +39,64 @@ def markdown_to_blocks(markdown):
         if split != "":
             splits.append(split)
     return splits
+
+
+def markdown_to_html_node(markdown):
+    children = []
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        block_node = block_to_nodes(block, block_type)
+        children.append(block_node)
+    return HTMLNode("div", None, children, None)
+
+
+def block_to_nodes(block, block_type):
+    match block_type:
+        case BlockType.CODE:
+            return HTMLNode("code", block[3:-3], None, None)
+        case BlockType.HEADING:
+            level = 6
+            for char in block[1:6]:
+                if char == "#":
+                    level -= 1
+                else:
+                    break
+            return HTMLNode(f"h{level}", block[8-level:], None, None)
+        case BlockType.QUOTE:
+            dirty = block.split()
+            clean = []
+            for line in dirty:
+                clean.append(line[2:])
+            cleaned = "\n".join(clean)
+            text_nodes = text_to_textnodes(cleaned)
+            children = []
+            for node in text_nodes:
+                children.append(text_node_to_html_node(node))
+            return HTMLNode("q", None, children, None)
+        case BlockType.UNORDERED_LIST:
+            lines = block.split()
+            children = []
+            for line in lines:
+                text_nodes = text_to_textnodes(line[2:])
+                html_nodes = []
+                for node in text_nodes:
+                    html_nodes.append(text_node_to_html_node(node))
+                children.append(HTMLNode("li", None, html_nodes, None))
+            return HTMLNode("ul", None, children, None)
+        case BlockType.ORDERED_LIST:
+            lines = block.split()
+            children = []
+            for line in lines:
+                text_nodes = text_to_textnodes(line[3:])
+                html_nodes = []
+                for node in text_nodes:
+                    html_nodes.append(text_node_to_html_node(node))
+                children.append(HTMLNode("li", None, html_nodes, None))
+            return HTMLNode("ol", None, children, None)
+        case _:
+            text_nodes = text_to_textnodes(block)
+            children = []
+            for node in text_nodes:
+                children.append(text_node_to_html_node(node))
+            return HTMLNode("p", None, children, None)
